542ad7cf20213ea22311e317aab6f896a67d2794
Unn2007 <118598200+Unn2007@users.noreply.github.com>
2024-12-21 17:51:47 +0200

    add persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {campersReducer} from './campers/slice.js';
 import { filtersReducer } from "./filters/slice.js";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const campersPersistConfig = {
+  key: "campers",
+  storage,
+  whitelist: ['isFavorite'],
+};
 export const store = configureStore({
   reducer: {
-    campers: campersReducer,
+    campers: persistReducer(campersPersistConfig, campersReducer),
+     
     filters: filtersReducer,
    
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+
+
+
+export const persistor = persistStore(store);
